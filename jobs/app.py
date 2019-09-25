c785cf6828b8bdbd4b817956694e9591149afede
Matt Laird <mlaird@10thmagnitude.com>
2019-09-25 13:51:54 -0500

    Add SQL lookup

--- jobs/app.py
+++ jobs/app.py
@@ -1,3 +1,4 @@
+
 import sqlite3
 from flask import Flask, render_template, g
 
@@ -35,4 +36,6 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql(
+        'SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
